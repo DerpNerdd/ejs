7f18e10937ad9ffc9c594e5b722fd6f04de6c408
DerpNerd <sanchez1.alan1@gmail.com>
2024-09-12 15:20:26 -0700

    Aaa

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
 var express = require('express');
 var app = express();
-const port = 5000
+const PORT = 5100
 
 //Imported Data
+const peopleData = require('./data/people')
+console.log(peopleData)
+app.set('view engine', 'ejs');  
+
+app.get('/', function(req, res) {
+    let judgement = "Nerds will inherit the earth"
+    res.render('pages/index',{
+        people: peopleData,
+        tagline: judgement
+    });
+})
+
+// app.get('/about', function(req, res) {
+//     res.render('pages/about');
+// })
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
